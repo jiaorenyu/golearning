0ca7bb2ab940beaea9646d8ada7df22846320a5a
jiaorenyu <jiaorenyu@miaozhen.com>
2017-02-06 10:57:28 +0800

    init

--- http/web_server.go
+++ http/web_server.go
@@ -1,13 +1,11 @@
-
 package main
 
 import (
-		"fmt"
-		"log"
-		"net/http"
-	   )
-
-
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"log"
+	"net/http"
+)
 
 type String string
 
@@ -18,26 +16,42 @@ type Struct struct {
 }
 
 func (str String) ServeHTTP(
-		w http.ResponseWriter,
-		r *http.Request) {
+	w http.ResponseWriter,
+	r *http.Request) {
 	fmt.Fprint(w, str)
+	value := r.FormValue("test")
+	fmt.Println(value)
 }
 
 func (st Struct) ServeHTTP(
-		w http.ResponseWriter,
-		r *http.Request) {
+	w http.ResponseWriter,
+	r *http.Request) {
 	fmt.Fprint(w, st.Greeting)
 	fmt.Fprint(w, st.Punct)
 	fmt.Fprint(w, st.Who)
 }
 
-func main() {
-	http.Handle("/string", String("I'm a frayed knot."))
-	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
-		err := http.ListenAndServe("localhost:4000", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Error")
+	}
+
+	defer c.Close()
+
+	reply, err := redis.String(c.Do("get", "a"))
+
+	//fmt.Println(reply)
+
+	//fmt.Println(r.FormValue("key"))
+	//fmt.Println(r.UserAgent())
+	fmt.Fprint(w, reply)
 }
 
-
+func main() {
+	http.HandleFunc("/get", getHandler)
+	err := http.ListenAndServe("localhost:4000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
